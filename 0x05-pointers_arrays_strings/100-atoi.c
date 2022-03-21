8e7503647bed76fb1e355b003a925fff1142a957
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-03-21 22:46:19 +0300

    Monday, Monday, so good to me; Monday morning, it was all I hoped it would be 😊

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,35 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
  */
-int main(void)
+int _atoi(char *s)
 {
-  int n;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-  n = _atoi("98");
-  printf("%d\n", n);
-  n = _atoi("-402");
-  printf("%d\n", n);
-  n = _atoi("          ------++++++-----+++++--98");
-  printf("%d\n", n);
-  n = _atoi("214748364");
-  printf("%d\n", n);
-  n = _atoi("0");
-  printf("%d\n", n);
-  n = _atoi("Suite 402");
-  printf("%d\n", n);
-  n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-  printf("%d\n", n);
-  n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-  printf("%d\n", n);
-  return (0);
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }

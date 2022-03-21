cc09e143f02ceaa3f4800237aa8774c64aa8cfa0
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-03-21 23:09:52 +0300

    A little comeback for thr Loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * print_number - prints an integer
+ * @n: tracked var
  */
+
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+		x = -x;
 	}
-	else
+	if ((x / 10) > 0)
 	{
-		m = n;
+		print_number(x / 10);
 	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	_putchar((x % 10) + 48);
 }

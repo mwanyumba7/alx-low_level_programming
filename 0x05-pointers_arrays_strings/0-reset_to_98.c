5e92cae552c4ee7a9d31f9975fe6e5c72fa7fab5
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-03-21 21:00:02 +0300

    Monday, Monday, so good to me; Monday morning, it was all I hoped it would be 😊

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,31 +1,10 @@
-#include <stdio.h>
-/*#include "holberton.h"*/
-void reset_to_98(int *n);
+#include "main.h"
 /**
- * main: prints an integer
- * @n: variable of type integer
- *
- * Return: Always 0
- */
-int main(void)
-{
-  int n, *m;
-  n = 402;
-  m = &n;
-  putchar(n);
-  reset_to_98(m);
-  putchar(n);
-  return (0);
-}
-
-/**
- * reset_to_98: prints an integer
- * @*n: pointer variable of type integer
- *
- * Return: void
+ * reset_to_98 - updates the value of the variable
+ * of pointer to 98
+ * @n: pointer to the variable to update
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
-    
+	*n = 98;
 }

6cb6aba1000291e07272fcd9ed30f42bd3831ab4
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-03-21 22:23:04 +0300

    Monday, Monday, so good to me; Monday morning, it was all I hoped it would be 😊

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * rev_string - function that reverses a string.
- * @s: declaration of *s and paramters for the function
- * Return: Always 0.
+ * rev_string - reverses a string
+ * @s: string to be reversed
  */
 void rev_string(char *s)
 {
-	int f, r_s;
-	char character;
+	char tmp;
 
-	for (f = '\0'; s[f] != 0; f++)
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
+		len++;
 	}
-	r_s = 0;
-	for (f = f - 1; r_s < f; r_s++)
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
 	{
-		character = s[f];
-		s[f] = s[r_s];
-		s[r_s] = character;
-		f--;
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }

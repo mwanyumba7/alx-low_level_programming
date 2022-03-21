6e45d082d73a1eb042826b7a5ab1e352f10993c8
Brayan Kai Mwanyumba <mwanyumbabrayan@gmail.com>
2022-03-21 22:32:01 +0300

    Monday, Monday, so good to me; Monday morning, it was all I hoped it would be 😊

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,24 +1,25 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * puts_half - Prints half of a string.
- * @str: The string to be printed.
+ * puts_half - a function that prints half of a string, followed by a new line.
+ * @str: An input string
+ * Return: Nothing
  */
 void puts_half(char *str)
 {
-	int index = 0, len = 0, n;
+	int len = 0, i, n;
 
-	while (str[index++])
+	while (str[len] != '\0')
 		len++;
 
-	if ((len % 2) == 0)
+	if (len % 2 == 0)
 		n = len / 2;
 
 	else
 		n = (len + 1) / 2;
 
-	for (index = n; index < len; index++)
-		_putchar(str[index]);
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
 
 	_putchar('\n');
 }
